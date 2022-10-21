d7898d72b3f79cff0be792dfc01a3fafab863742
Rafael Cena <rafael.cena@hotmail.com>
2022-10-21 07:43:24 -0300

    button

--- src/screens/home/style.ts
+++ src/screens/home/style.ts
@@ -23,5 +23,18 @@ export const styles = StyleSheet.create({
     color: '#FFFF',
     padding: 16,
     fontSize: 16,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24
+  },
+  button: {
+    width: 56,
+    marginTop: 5,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
