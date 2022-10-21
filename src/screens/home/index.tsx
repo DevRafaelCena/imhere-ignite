5188285cf1ee32f510e5be16e929dadebc89de0e
Rafael Cena <rafael.cena@hotmail.com>
2022-10-21 19:58:08 -0300

    component participante

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,5 @@
 import { Text, TextInput, View,TouchableOpacity } from "react-native";
-
+import { Participant } from "../../components/Participant";
 import { styles } from "./style";
 
 export function Home() {
@@ -9,29 +9,32 @@ export function Home() {
     }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
+      <View style={styles.container}>
+        <Text style={styles.eventName}>
+          Nome do evento
+        </Text>
 
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
+        <Text style={styles.eventDate}>
+          Sexta, 4 de Novembro de 2022.
+        </Text>
 
-    <View style={styles.form}>
-        <TextInput 
-            style={styles.input}
-            placeholder="Nome do participante"
-            placeholderTextColor="#6B6B6B"
-        />
+        <View style={styles.form}>
+            <TextInput 
+                style={styles.input}
+                placeholder="Nome do participante"
+                placeholderTextColor="#6B6B6B"
+            />
 
-        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-            <Text style={styles.buttonText}>
-            +
-            </Text>
-        </TouchableOpacity>
-    </View>
-    </View>
+            <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                <Text style={styles.buttonText}>
+                +
+                </Text>
+            </TouchableOpacity>        
+        </View>
+
+        <Participant />
+
+      </View>
   )
 }
 
