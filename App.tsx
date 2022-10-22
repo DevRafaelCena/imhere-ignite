c6fed035158249b0c0788cb4358ee7c08f228767
Rafael Cena <rafael.cena@hotmail.com>
2022-10-22 07:58:41 -0300

    finalizando primeiro modulo

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
-import {Home} from './src/screens/home'
+import { StatusBar } from "react-native";
+
+import { Home } from "./src/screens/home";
 
 export default function App() {
   return (
-    <Home/>
+    <>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent
+      />
+      <Home />
+    </>
   )
 }
--- src/screens/home/style.ts
+++ src/screens/home/style.ts
@@ -17,21 +17,21 @@ export const styles = StyleSheet.create({
     fontSize: 16
   },
   input: {
+    flex: 1,
     height: 56,
     backgroundColor: '#1F1E25',
     borderRadius: 5,
     color: '#FFFF',
     padding: 16,
     fontSize: 16,
-    flex: 1,
-    marginRight:12
+    marginRight: 12
   },
   buttonText: {
     color: '#FFF',
     fontSize: 24
   },
   button: {
-    width: 56,    
+    width: 56,
     height: 56,
     borderRadius: 5,
     backgroundColor: '#31CF67',
@@ -39,9 +39,14 @@ export const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   form: {
-    width:'100%',
+    width: '100%',
     flexDirection: 'row',
-    marginTop:36,
+    marginTop: 36,
     marginBottom: 42
+  },
+  listEmptyText: {
+    color: '#FFF',
+    fontSize: 14,
+    textAlign: 'center'
   }
 })
